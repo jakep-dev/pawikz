b86471c1cd58b5650326481066ceea5f9d79389a
Sherin Dharmarajan <sherinkd@live.com>
2016-01-11 11:22:26 -0500

    Sprint 3:- Handled login and errors

---
+++ src/app/main/pages/errors/404/error-404.module.js
@@ -0,0 +1,31 @@
+(function ()
+{
+    'use strict';
+
+    angular
+        .module('app.pages.error-404', [])
+        .config(config);
+
+    /** @ngInject */
+    function config($stateProvider, $translatePartialLoaderProvider)
+    {
+        // State
+        $stateProvider.state('app.pages_errors_error-404', {
+            url      : '/pages/errors/error-404',
+            views    : {
+                'main@'                             : {
+                    templateUrl: 'app/core/layouts/content-only.html'
+                },
+                'content@app.pages_errors_error-404': {
+                    templateUrl: 'app/main/pages/errors/404/error-404.html',
+                    controller : 'Error404Controller as vm'
+                }
+            },
+            bodyClass: 'error-404'
+        });
+
+
+        $translatePartialLoaderProvider.addPart('app/main/pages/errors/404');
+    }
+
+})();
