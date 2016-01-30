1e88c97b2ca7caacc451d46335f824bdfea74bd9
Sherin Dharmarajan <sherinkd@live.com>
2016-01-30 10:28:38 -0500

    Sprint 3:- Resolved a show stopper issue.

--- src/app/blocks/interceptor/interceptor.js
+++ src/app/blocks/interceptor/interceptor.js
@@ -8,61 +8,64 @@
 
     angular
         .module('blocks.interceptor')
-        .factory('interceptor', ['$q', '$location', '$rootScope', 'store',function($q, $location, $rootScope, store){
+        .factory('interceptor',ConfigInterceptor)
 
 
-            return {
-                response: function(response){
-                    $rootScope.isOperation = false;
-                    return response || $q.when(response);
-                },
-                request: function(request)
+
+    /** @ngInject */
+    function ConfigInterceptor($q, $location, $rootScope, store){
+        return {
+            response: function(response){
+                $rootScope.isOperation = false;
+                return response || $q.when(response);
+            },
+            request: function(request)
+            {
+                console.log('Request ---');
+                $rootScope.isOperation = true;
+                request.headers['x-session-token'] = store.get('x-session-token');
+                return request || $q.when(request);
+            },
+            requestError: function(rejection)
+            {
+                //store.remove('x-session-token');
+                //if(rejection.status == 401)
+                //{
+                //    $location.url('/pages/auth/login');
+                //}
+
+
+                return rejection || $q.when(rejection);
+            },
+            responseError: function(rejection)
+            {
+                console.log('Response Error');
+                $rootScope.isOperation = false;
+
+                store.remove('x-session-token');
+
+                if(rejection.status === 401)
                 {
-                    console.log('Request ---');
-                    $rootScope.isOperation = true;
-                    request.headers['x-session-token'] = store.get('x-session-token');
-                    return request || $q.when(request);
-                },
-                requestError: function(rejection)
+                    console.log('Inside -- 401');
+                    $location.url('/pages/auth/login');
+                }
+                else if(rejection.status === 500)
                 {
-                    //store.remove('x-session-token');
-                    //if(rejection.status == 401)
-                    //{
-                    //    $location.url('/pages/auth/login');
-                    //}
-
-
-                    return rejection || $q.when(rejection);
-                },
-                responseError: function(rejection)
+                    $location.url('/pages/errors/error-500')
+                }
+                else if(rejection.status === 404)
                 {
-                    console.log('Response Error');
-                    $rootScope.isOperation = false;
+                    $location.url('/pages/errors/error-404')
+                }
+                else {
 
-                    store.remove('x-session-token');
-
-                    if(rejection.status === 401)
-                    {
-                        console.log('Inside -- 401');
-                        $location.url('/pages/auth/login');
-                    }
-                    else if(rejection.status === 500)
-                    {
-                        $location.url('/pages/errors/error-500')
-                    }
-                    else if(rejection.status === 404)
-                    {
-                        $location.url('/pages/errors/error-404')
-                    }
-                    else {
-
-                    }
-
-                    return rejection || $q.when(rejection);
                 }
 
+                return rejection || $q.when(rejection);
             }
-        }]);
+
+        }
+    }
 
 
 })();
