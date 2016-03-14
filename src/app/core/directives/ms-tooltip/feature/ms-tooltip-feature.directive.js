c5a3591cfdc9c3d6b5d9d714bf4b49f15902d504
Po Lui <plui@advisen.com>
2016-03-14 10:43:26 -0400

    added closed tooltip attribute directive for use with md-option

---
+++ src/app/core/directives/ms-tooltip/feature/ms-tooltip-feature.directive.js
@@ -0,0 +1,22 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('app.core')
+    .directive('closeTooltips', [function () {
+        return {
+            restrict: 'A',
+            link: function (scope, element, attibutes) {
+                element.bind('click', function () {
+                    var obj = angular.element(document).find("md-tooltip");
+                    if (Array.isArray(obj)) {
+                        Array.forEach(function (o) {
+                            o.hide();
+                        });
+                    } else {
+                        obj.hide();
+                    }
+                })
+            }
+        }
+    }]);
+})();
