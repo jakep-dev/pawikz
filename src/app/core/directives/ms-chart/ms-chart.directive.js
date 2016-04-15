3af5571b91dde52903ced30581df6c5342185360
Pramod Paudel <pramoddevp5@gmail.com>
2016-04-15 12:59:59 -0400

    DropDown multiselect and Autosave fix

--- src/app/core/directives/ms-chart/ms-chart.directive.js
+++ src/app/core/directives/ms-chart/ms-chart.directive.js
@@ -17,7 +17,7 @@
     }
 
     /** @ngInject */
-    function msChartDirective($rootScope, $compile, stockService, commonBusiness)
+    function msChartDirective($rootScope, $compile, stockService, commonBusiness, toast, $interval, clientConfig)
     {
         return {
             restrict: 'E',
@@ -34,7 +34,7 @@
                 {
                     var html = '';
                     var newScope = null;
-
+                    toast.simpleToast("Auto Save Enabled");
                     switch (angular.lowercase(scope.type))
                     {
                         case 'stock':
@@ -331,11 +331,17 @@
 
                                         });
 
-                                        stockService.saveChartAllSettings(commonBusiness.companyId,
+                                        return stockService.saveChartAllSettings(commonBusiness.companyId,
                                             commonBusiness.stepId, commonBusiness.projectId, startArr);
                                     };
 
                                     scope.saveAllCharts = saveAllCharts;
+
+                                    $interval(function(){
+                                        saveAllCharts().then(function(){
+                                            toast.simpleToast("Saved Successfully");
+                                        });
+                                    }, 360000); /*clientConfig.appSettings.autoSaveTimeOut);*/
                                 });
                             break;
                         case 'bar':
