42f3a8db2ab9f828837f01e20e0611ba5ba655b0
Po Lui <plui@advisen.com>
2016-07-06 16:59:34 -0400

    Feature 26 Issue 23 Refactor KMB functionality Fixed the scenario where The visible text field has value of ###[KMB] and the prior value is the same as converted value of the current visible value(###[KMB]) The current code thinks there is no change in value and therefore the ###[KMB] doesn't convert to the actual number.
    For example prior value 2,000,000 and then user enters 2m, all the textchange and scope.watch was triggered but the text field value remains at 2m because the code thinks no value has changed.

--- src/app/core/directives/ms-template/templates/ms-program/textbox/ms-program-textbox.directive.js
+++ src/app/core/directives/ms-template/templates/ms-program/textbox/ms-program-textbox.directive.js
@@ -35,8 +35,7 @@
 
             //if the number pattern matches numeric shortcuts ###[kmb] then skip the computation of the rest of the rows
             //ms-numeric-shortcut directive will call textchange and trigger the $scope.$watch
-            var regEx = /^[0-9]+\.?[0-9]*[kKmMbB]$/;
-            if (!regEx.test(inputVal))
+            if (!templateBusiness.isKMBValue(inputVal))
             {
                 inputVal = removeCommaValue(inputVal);
                 if (inputVal) {
@@ -50,14 +49,32 @@
                     currentRow: $scope.row, value: $scope.value,
                     rowId: $scope.rowid, columnName: $scope.columnname
                 });
+
+                //In the scenario if the visible text field has value of ###[KMB] 
+                //and the prior value is the same as converted value of the current visible value(###[KMB])
+                //For example prior value 2,000,000 and then user enters 2m, all the textchange and scope.watch was triggered.
+                var element = $scope.sourceElement.find("input");
+                var visibleValue = element.val();
+                if (templateBusiness.isKMBValue(visibleValue))
+                {
+                    element.val($scope.value);
+                }
             }
-            else { console.log('Skipping over the procession of ' + inputVal); }
+            else { console.log('Skipping over the processing of ' + inputVal); }
         }
 
         $scope.$watch(
             "value",
             function handleAutoSave(newValue, oldValue) {
                 //console.log('$watch on scope.value --> newValue = "' + newValue + '"' + ' oldValue ="' + oldValue + '"');
+
+                //In the scenario if the visible text field has value of ###[KMB] 
+                //and the prior value is the same as converted value of the current visible value(###[KMB])
+                //scope.watch gets triggered with the 2m value and need to convert it
+                if (templateBusiness.isKMBValue(newValue))
+                {
+                    newValue = templateBusiness.transformKMB(newValue);
+                }
                 newValue = removeCommaValue(newValue);
                 oldValue = removeCommaValue(oldValue);
                 if (newValue !== oldValue)
@@ -87,9 +104,9 @@
             },
             controller: 'MsProgramTextController',
             templateUrl: 'app/core/directives/ms-template/templates/ms-program/textbox/ms-program-textbox.html',
-            link: function(scope)
+            link: function (scope, element)
             {
-
+                scope.sourceElement = element;
             }
         };
     }
