abefa67cf92991cbf0d61d30a06bee410760f74c
Sherin Dharmarajan <sherinkd@live.com>
2016-09-27 00:15:12 -0400

    Push the disconnect issue

--- src/app/core/directives/ms-window-exit/ms-window-exit.directive.js
+++ src/app/core/directives/ms-window-exit/ms-window-exit.directive.js
@@ -8,20 +8,10 @@
                     link: function(element, attrs){
                         if($state.current.name != 'app.pages_auth_login') {
                             var myEvent = $window.attachEvent || $window.addEventListener,
-                                chkevent = $window.attachEvent ? 'onbeforeunload' : 'beforeunload', /// make IE7, IE8 compatable
-                                chkunload = $window.attachEvent ? 'onunload' : 'unload';
+                                chkevent = $window.attachEvent ? 'onbeforeunload' : 'beforeunload' /// make IE7, IE8 compatable
 
                             myEvent(chkevent, function (e) { // For >=IE7, Chrome, Firefox
                                 clientConfig.socketInfo.socket.disconnect();
-                                overviewBusiness.save();
-                                templateBusiness.save();
-                                templateBusiness.saveTable();
-                                templateBusiness.saveHybridTable();
-
-                            });
-
-                            myEvent(chkunload, function (e) { // For >=IE7, Chrome, Firefox
-                                authBusiness.logOut();
                             });
                         }
                     }
