cf1eb805a5cc1d2563858c3b829f2a9aa6499d51
Sherin Dharmarajan <sherinkd@live.com>
2016-09-27 23:10:31 -0400

    Resolved the IE10 issue on lock workup

--- src/app/core/directives/ms-window-exit/ms-window-exit.directive.js
+++ src/app/core/directives/ms-window-exit/ms-window-exit.directive.js
@@ -8,7 +8,7 @@
                     link: function(element, attrs){
                         if($state.current.name != 'app.pages_auth_login') {
                             var myEvent = $window.attachEvent || $window.addEventListener,
-                                chkevent = $window.attachEvent ? 'onbeforeunload' : 'beforeunload' /// make IE7, IE8 compatable
+                                chkevent = $window.attachEvent ? 'onunload' : 'unload'; /// make IE7, IE8 compatable
 
                             myEvent(chkevent, function (e) { // For >=IE7, Chrome, Firefox
                                 clientConfig.socketInfo.socket.disconnect();
