52a2664c69b7c3da360d8ed9d0525ca226c009f3
Rex Domingo <rdomingo@advisen.com>
2016-11-11 15:19:28 +0800

    Checkboxes on Hudson D&O Work-up

---
+++ src/app/core/directives/ms-template/templates/ms-generic-select/ms-generic-select.directive.js
@@ -0,0 +1,67 @@
+(function ()
+{
+    'use strict';
+
+    angular
+        .module('app.core')
+        .controller('MsGenericSelectController', MsGenericSelectController)
+        .directive('msGenericSelect', msGenericSelectDirective);
+
+    /** @ngInject */
+    function MsGenericSelectController($scope, templateBusiness)
+    {
+        $scope.disabled = ($scope.isdisabled === 'true');
+        $scope.trueValue = '\'' + $scope.text + '\'';
+        $scope.isSelected =false;
+
+        $scope.checkChange = function()
+        {
+            $scope.value = $scope.isSelected;
+        };
+
+
+        $scope.$watch(
+            "tearsheetObj.selectedValue",
+            function handleAutoSave(newValue, oldValue) {
+                
+                // if($scope.isSelected)
+                // {
+                //      newValue = $scope.isSelected;
+                // }
+
+                if(newValue !== oldValue)
+                {
+                    templateBusiness.getReadyForAutoSave($scope.itemid, $scope.mnemonicid, newValue);
+                    templateBusiness.updateMnemonicValue($scope.itemid, $scope.mnemonicid, newValue);
+                }
+            }
+
+
+        );
+    }
+
+    /** @ngInject */
+    function msGenericSelectDirective()
+    {
+        return {
+            restrict: 'E',
+            scope   : {
+                row: '=',
+                isdisabled: '@',
+                text: '@',
+                ischecked: '@',
+                value : '@',
+                itemid : '@',
+                mnemonicid : '@',
+                tearsheet : '@'
+            },
+            controller: 'MsGenericSelectController',
+            templateUrl: 'app/core/directives/ms-template/templates/ms-generic-select/ms-generic-select.html',
+            link: function($scope)
+            {
+                 $scope.tearsheetObj = angular.fromJson(_.unescape($scope.tearsheet));
+            }
+        };
+    }
+
+})();
