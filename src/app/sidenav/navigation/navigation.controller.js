9f273a0ee219fd1b137138128e3d1697728f3520
Sherin Dharmarajan <sherinkd@live.com>
2017-03-23 00:11:11 -0400

    Resolved the userName issue

--- src/app/sidenav/navigation/navigation.controller.js
+++ src/app/sidenav/navigation/navigation.controller.js
@@ -7,12 +7,12 @@
         .controller('NavigationController', NavigationController);
 
     /** @ngInject */
-    function NavigationController($scope, store, navConfig, commonBusiness)
+    function NavigationController($scope, store, navConfig, $interval)
     {
         var vm = this;
 
         navConfig.sideNavItems = [];
-        vm.userName = '';
+        vm.userName = null;
         vm.sideNavItems = navConfig.sideNavItems;
 
         // Data
@@ -21,9 +21,19 @@
         };
 
         var userDetails = store.get('user-info');
+        var promiseUser = null;
 
         if(userDetails){
             vm.userName = userDetails.fullName;
+        }
+        else{
+            promiseUser = $interval(function(){
+                var userDetails = store.get('user-info');
+                if (userDetails) {
+                    vm.userName = userDetails.fullName;
+                    $interval.cancel(promiseUser);
+                }
+            }, 1000);
         }
 
     }
