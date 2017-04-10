971df11a510717e76e62efdce877178ee0d0809d
Rex Domingo <rdomingo@advisen.com>
2017-04-10 17:45:41 +0800

    UF-594 , Resolved - Work-up history logs unnecessary updates if textarea has default inline text.

--- src/app/core/directives/ms-template/templates/ms-rich-text-editor/ms-rich-text-editor.directive.js
+++ src/app/core/directives/ms-template/templates/ms-rich-text-editor/ms-rich-text-editor.directive.js
@@ -159,11 +159,20 @@
         $scope.$watch(
             "value",
             function handleAutoSave(newValue, oldValue) {
+                // check history logs unnecessary updates
+                newValue = removeHtmlTags(newValue);
+                oldValue = removeHtmlTags(oldValue);
                 if(newValue !== oldValue) {
                     templateBusinessSave.getReadyForAutoSave($scope.itemid, $scope.mnemonicid, newValue, clientConfig.uiType.general);
                 }
             }
         );
+    }
+
+    // check for default Text value purposed
+    function removeHtmlTags(value){
+        value = value ? String(value).replace(/<[^>]+>/gm, '') : ''; 
+        return value;
     }
 
     /** @ngInject */
