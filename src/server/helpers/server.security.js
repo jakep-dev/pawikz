3b6bf7ccab0a270f85cb1573a1b62f644727976a
Po Lui <plui@advisen.com>
2018-05-14 13:10:10 -0400

    Minor socket.io changes

--- src/server/helpers/server.security.js
+++ src/server/helpers/server.security.js
@@ -23,11 +23,19 @@
             res.locals._csrf = csrfToken;
 
             res.removeHeader("X-Powered-By");
-            if(connectSrc !== '') {
-                res.header('Content-Security-Policy', "script-src 'self' *.advisen.com 'unsafe-eval' 'unsafe-inline'; object-src 'self'; connect-src 'self' " + connectSrc);
+            var cspHeader = "default-src: 'self' ";
+            if(connectSrc.indexOf('wss:') >= 0) {
+                cspHeader += 'wss:;';
             } else {
-                res.header('Content-Security-Policy', "script-src 'self' *.advisen.com 'unsafe-eval' 'unsafe-inline'; object-src 'self'");
+                cspHeader += 'ws:;';
             }
+            if(connectSrc !== '') {
+                cspHeader += " script-src 'self' *.advisen.com 'unsafe-eval' 'unsafe-inline'; object-src 'self'; connect-src 'self' " + connectSrc;
+            } else {
+                cspHeader += " script-src 'self' *.advisen.com 'unsafe-eval' 'unsafe-inline'; object-src 'self'";
+            }
+            console.log(cspHeader);
+            res.header('Content-Security-Policy', cspHeader);
             res.header('Content-Security-Policy', "script-src 'self' *.advisen.com 'unsafe-eval' 'unsafe-inline'; object-src 'self'; connect-src 'self'");
             res.header('Strict-Transport-Security', 'max-age=31536000; includeSubDomains; preload');
             res.header('X-Frame-Options', 'SAMEORIGIN');
